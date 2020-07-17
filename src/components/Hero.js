3f70808f1186adcce04b0cbc44ad4305d01f81cc
Ben <46476247+hexagonatron@users.noreply.github.com>
2020-07-18 02:28:33 +0930

    Index done

---
+++ src/components/Hero.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Hero = () => {
+    return (
+        <div className="row">
+            <div className="col col-12 col-md-4 feature-section">
+                <div className="hero-text text-dark bold d-flex align-items-center justify-content-center flex-column text-center">
+                    <h1>Ben Fawcett</h1>
+                    <h3>Full-Stack Web Developer</h3>
+                </div>
+            </div>
+            <div className="col col-12 col-md-8 hero-image">
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
